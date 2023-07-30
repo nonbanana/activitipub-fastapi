9db78ce1b0bc257fc0170ee43408f6efbfe2a98f
hjcho <hjcho@mycreatz.com>
2023-07-30 16:38:14 +0900

    docker 사용시 버그 수정 및 response가 바이너리일경우 exception 수정

--- docker/api/startup.sh
+++ docker/api/startup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-dockerize -wait tcp://db:3306 -timeout 20s
-alembic upgrade head && gunicorn --bind 0.0.0.0:8000 -w 4 -k uvicorn.workers.UvicornWorker app.server:app
+if [ "$ENV" == "prod" ]
+then
+    # for production
+    alembic upgrade head && gunicorn --bind 0.0.0.0:$BACKEND_PORT -w 4 -k uvicorn.workers.UvicornWorker app.server:app
+else
+    dockerize -wait $DB_HOST -timeout 20s
+    # for development
+    alembic upgrade head && uvicorn app.server:app --host 0.0.0.0 --port $BACKEND_PORT --reload
+fi
